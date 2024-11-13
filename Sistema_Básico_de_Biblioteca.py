bf30dff2875c76864625e88dcec421ca2e434541
Marco Badany Eggers <mabadanye@gmail.com>
2024-11-12 21:30:27 -0400

    Se anadieron categorias al sistema

--- Sistema_Básico_de_Biblioteca.py
+++ Sistema_Básico_de_Biblioteca.py
@@ -1,6 +1,6 @@
-#Sistema Básico de Biblioteca
+# Sistema Básico de Biblioteca
 
-#Este programa permitirá agregar títulos de libros a la colección, 
+# Este programa permitirá agregar títulos de libros a la colección, 
 # eliminar libros y verificar si un libro está en la colección. 
 # Usaremos un conjunto (set) para almacenar los títulos de libros, 
 # ya que los conjuntos solo guardan elementos únicos, evitando duplicados automáticamente.
@@ -30,3 +30,27 @@ print("The Data Science Handbook: Advice and Insights from 25 Amazing Data Scien
 
 print("¿Está 'Doing Data Science: Straight Talk from the Frontline' en la colección?")
 print("Doing Data Science: Straight Talk from the Frontline" in biblioteca)
+
+# Colecciones de libros
+novedades = {'El Poder del Presente', 'Mindset: La Nueva Psicología del Éxito', 'Atomic Habits: Cómo crear hábitos buenos y romper los malos'}
+recomendados = {'El Instituto', 'La Fórmula de la Felicidad', 'El Código del Universo'}
+
+# Unión: Todos los libros en ambas colecciones
+todos_libros = novedades.union(recomendados)
+print("Todos los libros:", todos_libros)
+
+# Intersección: Libros en común entre ambas colecciones
+libros_comunes = novedades.intersection(recomendados)
+print("Libros en ambas colecciones:", libros_comunes)
+
+# Diferencia: Libros en 'Novedades' que no están en 'Recomendados'
+solo_novedades = novedades.difference(recomendados)
+print("Solo en Novedades:", solo_novedades)
+
+# Diferencia Simétrica: Libros que están en una colección pero no en ambas
+libros_unicos = novedades.symmetric_difference(recomendados)
+print("Libros exclusivos de cada colección:", libros_unicos)
+
+# Unión: Todos los libros en ambas colecciones
+todos_libros = novedades.union(recomendados)
+print("Todos los libros:", todos_libros)
