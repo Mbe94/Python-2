d451347b8811ba38c063cdef4bb2f7a18922ab53
Marco Badany Eggers <mabadanye@gmail.com>
2024-11-13 23:17:20 -0400

    Listas con nuevos ejemplos

--- Lists.py
+++ Lists.py
@@ -1,7 +1,16 @@
-numbers = []
-for element in range(1, 11):
-  numbers.append(element* 2)
-print(numbers )
+#numbers = []
+#for element in range(1, 11):
+#  numbers.append(element* 2)
+#print(numbers )
 
-numbers_V2 = [element *2 for element in range(1, 11)]
+#numbers_V2 = [element *2 for element in range(1, 11)]
+#print(numbers_V2)
+
+numbers = []
+for i in range(1, 11):
+    if i % 2 == 0:
+        numbers.append(i*2)
+print(numbers)
+
+numbers_V2 = [i *2 for i in range(1, 11) if i % 2 == 0]
 print(numbers_V2)
