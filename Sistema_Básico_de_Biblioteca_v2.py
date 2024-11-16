24bb8c8925db67c5532fa9e92b24b27336e2050e
Marco Badany Eggers <mabadanye@gmail.com>
2024-11-16 13:32:42 -0400

    Nueva versión

---
+++ Sistema_Básico_de_Biblioteca_v2.py
@@ -0,0 +1,9 @@
+# Sistema Básico de Biblioteca
+
+#Este programa te permitirá almacenar libros
+
+# Creamos un diccionario para almacenar los libros
+biblioteca = {"El Poder del Ahora":"Eckhart Tolle","Los 7 Hábitos de la Gente Altamente Efectiva":"Stephen Covey","El Secreto":"Rhonda Byrne" }
+print(biblioteca)
+
+
