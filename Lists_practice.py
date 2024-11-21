1c302db84d59b31076e68f91e27b3851ded71919
Marco Badany Eggers <mabadanye@gmail.com>
2024-11-20 21:48:26 -0400

    Añadi ejercicios de nivel 2

--- Lists_practice.py
+++ Lists_practice.py
@@ -95,3 +95,60 @@ print(valpos)
 
 valpos = [i * 5 for i in range(1,11) ]
 print(valpos)
+
+
+#----------------------------------------
+
+#Duplicar los valores de una lista: Dada la lista numbers = [1, 2, 3, 4, 5], crea una nueva lista donde cada número sea multiplicado por 2.
+
+numbers2 = [1,2,3,4,5]
+newnumbers2 = [i * 2 for i in numbers2 ]
+print(newnumbers2)
+
+#----------------------------------------
+
+#Filtrar números mayores que 5: Dada la lista numbers = [3, 4, 5, 6, 7, 8], crea una nueva lista que contenga solo los números mayores que 5.
+
+numbers3 = [3,4,5,6,7,8]
+newnumbers3 = [i for i in numbers3 if i > 5 ]
+print(newnumbers3)
+
+#----------------------------------------
+
+#Obtener las primeras letras de los nombres: Dada la lista names = ["Ana", "Juan", "Pedro", "Maria"], crea una nueva lista con solo las primeras letras de los nombres.
+
+names = ["Ana", "Juan", "Pedro", "Maria"]
+primeras_letras = [nombre[0] for nombre in names]
+print(primeras_letras)
+
+#----------------------------------------
+
+#Filtrar los números impares: Dada la lista numbers = [1, 2, 3, 4, 5, 6], crea una nueva lista que contenga solo los números impares.
+
+numbers4 = [1, 2, 3, 4, 5, 6]
+newnumbers4 = [i for i in numbers4 if i % 2 != 0]
+print(newnumbers4)
+
+#----------------------------------------
+
+#Multiplicar por 3 y filtrar números menores que 10: Dada la lista numbers = [1, 3, 5, 7, 9], crea una nueva lista con los números multiplicados por 3 que sean menores que 10.
+
+numbers5 = [1, 3, 5, 7, 9]
+newnumbers5 = [i for i in numbers5 if i * 3 < 10  ]
+print(newnumbers5)
+
+#----------------------------------------
+
+#Filtrar palabras que contienen una letra específica: Dada la lista words = ["banana", "manzana", "melon", "pera"], genera una nueva lista que contenga las palabras que contienen la letra "a".
+
+words = ["banana", "manzana", "melon", "pera"]
+palabras_con_a = [palabra for palabra in words if 'a' in palabra]
+print(palabras_con_a)
+
+#----------------------------------------
+
+#Crear una lista con el cuadrado de los números divisibles por 3: Dada la lista numbers = [1, 2, 3, 4, 5, 6, 9, 12], crea una nueva lista que contenga el cuadrado de los números que son divisibles por 3.
+
+numbers6 = [1, 2, 3, 4, 5, 6, 9, 12]
+newnumbers6 = [i*i for i in numbers6 if i % 3 ==0]
+print(newnumbers6)
