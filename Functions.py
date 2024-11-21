1c35d6a3244ab9dde1accd60e1dff4b87e8d2f13
Marco Badany Eggers <mabadanye@gmail.com>
2024-11-20 22:23:07 -0400

    Mis primeras funciones

---
+++ Functions.py
@@ -0,0 +1,27 @@
+print('Hola')
+
+def my_print():
+  print('This is my print')
+  print('This is my print 2')
+
+my_print()
+
+def my_print(text):
+  print(text * 2)
+
+my_print('Este es mi texto')
+my_print('Hola')
+
+a = 10
+b = 90
+c = a + b
+
+def suma(a, b):
+  print(a + b)
+suma(1, 5)
+suma(10, 4)
+
+def suma(a, b):
+  my_print(a + b)
+suma(1, 5)
+suma(10, 4)
