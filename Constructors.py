a587650ce49e9696eee54bfa9e4d147c0af8b1d9
Marco Badany Eggers <mabadanye@gmail.com>
2024-11-26 21:55:25 -0400

    Constructores usados en Python

---
+++ Constructors.py
@@ -0,0 +1,21 @@
+#Estos son herramientas muy prácticas para manipular datos de manera eficiente y son especialmente útiles en comprensiones:
+
+#-----------------------------------------------------------------------------------------
+range()
+#¿Qué hace?: Genera una secuencia de números, que puede ser usada para iterar en bucles o comprensiones.
+
+#Sintáxis
+range(start, stop, step)
+
+#Ejemplo
+numeros = [x for x in range(5)]  # [0, 1, 2, 3, 4]
+
+#-----------------------------------------------------------------------------------------
+zip()
+#¿Qué hace?: Genera una secuencia de números, que puede ser usada para iterar en bucles o comprensiones.
+
+#Sintáxis
+range(start, stop, step)
+
+#Ejemplo
+numeros = [x for x in range(5)]  # [0, 1, 2, 3, 4]
