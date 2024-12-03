fa1509186515e69a39491a0b540dc738b92bea78
Marco Badany Eggers <mabadanye@gmail.com>
2024-12-03 15:29:13 -0400

    Ejercicios de Return functions

---
+++ Functions_return_practice.py
@@ -0,0 +1,21 @@
+# Ejercicios Nivel 1
+# Suma de dos números: Escribe una función suma que tome dos argumentos (números) y retorne su suma.
+
+def suma(a,b):
+    return a+b
+
+resultado = suma(10,15)
+print(resultado)
+
+#---------------------------------------------------------------------------------------------------
+
+# Área de un triángulo
+# Crea una función area_triangulo que reciba la base y la altura de un triángulo y retorne su área.
+
+def area_triangulo(base,altura):
+    return ((base * altura)/2)
+
+resultado_area = area_triangulo(5,10)
+print(resultado_area)
+
+#---------------------------------------------------------------------------------------------------
