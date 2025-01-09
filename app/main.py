20ade1a5de9213a37d36a0016997d742d6f1a5c7
Marco <mabadanye@gmail.com>
2025-01-09 05:55:07 -0400

    Finalización del Curso

--- app/main.py
+++ app/main.py
@@ -1,23 +1,17 @@
 import utils
+import read_csv
+import charts
 
-data =[
-  {
-    'Country': 'Colombia',
-    'Population': 500 
-  },
-  {
-    'Country': 'Bolivia',
-    'Population': 300
-  }
-]
 def run():
-  keys, values = utils.get_population()
-  print(keys, values)
-  
-  country = input( 'Type Country => ')
-  
+  data = read_csv.read_csv('./app/data.csv')
+  country = input('Type Country => ')
+
   result = utils.population_by_country(data, country)
-  print(result)
+
+  if len(result) > 0:
+    country = result[0]
+    labels, values = utils.get_population(country)
+    charts.generate_bar_chart(labels,values)
 
 if __name__ == '__main__':
   run()
